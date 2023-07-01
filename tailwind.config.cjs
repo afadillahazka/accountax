754d91b1e814d009a632117ba2ccdb795599764b
Aditya Fadillah Azka <afadillahazka@gmail.com>
2023-07-01 17:53:52 +0700

    update 010723 2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,14 @@ module.exports = {
     colors: {
       tosca: "#4cb2a9",
       toscalight: "#7FE5DC",
-      red: "#EC3A28",
+      toscalight_2: "#54c0a9",
+      red: "#FF312E",
+      orange: "#fbc70d",
       white: "rgb(255 255 255)",
-      black: "rgb(0, 0, 0)"
+      black: "rgb(0, 0, 0)",
+      gray: "#E1E1E1",
+      gradient: "rgb(249,180,24)",
+      mint: "#F0F7EE"
     },
     extend: {
       fontFamily: {
